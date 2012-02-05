183989575aa732dbb0251b9a3cee184e754db4d8
Arnold Roa <manolet@gmail.com>
2012-02-05 09:26:35 -0500

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leadbox::Application.config.secret_token = '52adf2b418d66c4a70ff2971df73d0868c2b1b5b91e15d29b8591ed94a5ccbda8d59f300e13e027611394ee1ac3c331f8e11eaa397aa4129ae31cc4577e381b8'
